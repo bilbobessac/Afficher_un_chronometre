4d2bd9756845c5180f0d3bcf2cd3878b6ddd22c7
bilbobessac <74983423+bilbobessac@users.noreply.github.com>
2022-01-11 20:25:20 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 input.onButtonPressed(Button.A, function () {
     while (input.buttonIsPressed(Button.A)) {
+        basic.showNumber(tps_chrono)
         tps_chrono += 1
         basic.pause(100)
-        basic.showNumber(tps_chrono)
     }
     basic.showNumber(tps_chrono)
 })

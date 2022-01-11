3f0ac549f573fdc5d6700a44cd36156503920d65
bilbobessac <74983423+bilbobessac@users.noreply.github.com>
2022-01-11 19:27:30 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-
+input.onButtonPressed(Button.A, function () {
+    while (input.buttonIsPressed(Button.A)) {
+        tps_chrono += 1
+        basic.pause(100)
+        basic.showNumber(tps_chrono)
+    }
+    basic.showNumber(tps_chrono)
+})
+input.onButtonPressed(Button.B, function () {
+    tps_chrono = 0
+    basic.showNumber(tps_chrono)
+})
+let tps_chrono = 0
+basic.clearScreen()
+tps_chrono = 0
+basic.showNumber(tps_chrono)
